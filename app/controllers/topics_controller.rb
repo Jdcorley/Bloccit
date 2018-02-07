a067657e35b9d7c9c9c187c40125f63eaf667817
Jack <jdcorley@gmail.com>
2018-02-07 17:55:40 -0500

    assignment-30

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,69 +3,69 @@ class TopicsController < ApplicationController
   before_action :require_sign_in, except: [:index, :show]
 
   before_action :authorize_user, except: [:index, :show]
-# INDEX
- def index
-  @topics = Topic.all
- end 
-# SHOW
- def show
-  @topic = Topic.find(params[:id])
- end
-# NEW
- def new 
-  @topic = Topic.new
- end
-# CREATE
- def create 
-  @topic = Topic.new(topic_params)
-    
-  if @topic.save
-    redirect_to @topic, notice: "Topic was saved successfully."
-  else 
-   flash.now[:alert] = "Error creating topic. Please try again."
-   render :new
-  end 
- end
-# EDIT
- def edit
-  @topic = Topic.find(params[:id])
- end 
-# UPDATE
- def update 
-  @topic = Topic.find(params[:id])
-  
-  @topic.assign_attributes(topic_params)
+  # INDEX
+  def index
+    @topics = Topic.all
+  end
+  # SHOW
+  def show
+    @topic = Topic.find(params[:id])
+  end
+  # NEW
+  def new
+    @topic = Topic.new
+  end
+  # CREATE
+  def create
+    @topic = Topic.new(topic_params)
 
-  if @topic.save
-  flash[:notice] = "Topic was updated."
-  redirect_to @topic
-  else 
-  flash.now[:alert] = "Error saving topic. Please try again."
-  render :edit
-  end 
- end
- # DESTROY
- def destroy 
-  @topic = Topic.find(params[:id])
-  
-  if @topic.destroy 
-   flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-   redirect_to action: :index
-  else 
-   flash.now[:alert] = "There was an error deleting the topic."
-   render :show
-  end 
- end 
+    if @topic.save
+      redirect_to @topic, notice: 'Topic was saved successfully.'
+    else
+      flash.now[:alert] = 'Error creating topic. Please try again.'
+      render :new
+    end
+  end
+  # EDIT
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+  # UPDATE
+  def update
+    @topic = Topic.find(params[:id])
 
- private
- def topic_params
-  params.require(:topic).permit(:name, :description, :public)
- end 
+    @topic.assign_attributes(topic_params)
 
- def authorize_user 
-  unless current_user.admin?
-    flash[:alert] = "You must be an admin to do that."
-    redirect_to topics_path 
-    end 
-  end 
-end 
+    if @topic.save
+      flash[:notice] = 'Topic was updated.'
+      redirect_to @topic
+    else
+      flash.now[:alert] = 'Error saving topic. Please try again.'
+      render :edit
+    end
+  end
+  # DESTROY
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = '\'#{@topic.name}\' was deleted successfully.'
+      redirect_to action: :index
+    else
+      flash.now[:alert] = 'There was an error deleting the topic.'
+      render :show
+    end
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
+
+  def authorize_user
+    unless current_user.admin?
+      flash[:alert] = 'You must be an admin to do that.'
+      redirect_to topics_path
+    end
+  end
+end
--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -3,74 +3,74 @@ include RandomData
 
 RSpec.describe AdvertisementsController, type: :controller do
 
-  let (:my_ad) do 
-      Advertisement.create(
-        id: 1,
-        title: RandomData.random_sentence,
-        copy: RandomData.random_paragraph,
-        price: 99
-      )      
-  end  
+  let (:my_ad) do
+    Advertisement.create(
+      id: 1,
+      title: RandomData.random_sentence,
+      copy: RandomData.random_paragraph,
+      price: 99
+    )
+  end
   # GET index
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
 
-    it "assigns [my_ad] to @advertisements" do 
-      get :index 
+    it 'assigns [my_ad] to @advertisements' do
+      get :index
       expect(assigns(:advertisements)).to eq([my_ad])
-    end 
+    end
   end
   # GET show
-  describe "GET #show" do 
-    it "returns http success" do 
+  describe 'GET #show' do
+    it 'returns http success' do
       get :show, params: { id: my_ad.id }
       expect(response).to have_http_status(:success)
-    end 
+    end
 
-    it "renders the #show view" do 
+    it 'renders the #show view' do
       get :show, params: { id: my_ad.id }
-      expect(response).to render_template :show 
-    end 
+      expect(response).to render_template :show
+    end
 
-    it "assigns my_ad to @advertisement" do 
+    it 'assigns my_ad to @advertisement' do
       get :show, params: { id: my_ad.id }
       expect(assigns(:advertisement)).to eq(my_ad)
-    end 
-  end 
-  # GET new 
-  describe "GET new" do 
-    it "returns http success" do 
-      get :new 
+    end
+  end
+  # GET new
+  describe 'GET new' do
+    it 'returns http success' do
+      get :new
       expect(response).to have_http_status(:success)
-    end 
+    end
 
-    it "renders the #new view" do 
-      get :new 
-      expect(response).to render_template :new 
-    end 
+    it 'renders the #new view' do
+      get :new
+      expect(response).to render_template :new
+    end
 
-    it "instantiates @advertisement" do 
-      get :new 
+    it 'instantiates @advertisement' do
+      get :new
       expect(assigns(:advertisement)).not_to be_nil
-    end 
-  end 
+    end
+  end
   # Post Create
-  describe "POST create" do 
-    it "increase the number of advertisements by 1" do 
+  describe 'POST create' do
+    it 'increase the number of advertisements by 1' do
       expect{ post :create, params: {advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99} } }.to change(Advertisement,:count).by(1)
     end
 
-    it "assigns the new advertisement to @advertisement" do 
+    it 'assigns the new advertisement to @advertisement' do
       post :create, params: {advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}}
-      expect(assigns(:advertisement)).to eq Advertisement.last 
-    end 
+      expect(assigns(:advertisement)).to eq Advertisement.last
+    end
 
-    it "redirects to the new advertisement" do 
+    it 'redirects to the new advertisement' do
       post :create, params: {advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}}
-      expect(response).to redirect_to Advertisement.last 
-    end 
-  end 
+      expect(response).to redirect_to Advertisement.last
+    end
+  end
 end
