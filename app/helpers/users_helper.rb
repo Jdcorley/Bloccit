560433f539d12d0d040c174edd905881276d51f6
Jack <jdcorley@gmail.com>
2018-02-12 13:12:31 -0500

    assignment-31

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  
+  def current_user_posts_none
+    current_user.posts.length == 0 
+  end 
+
+  def current_user_comments_none
+    current_user.comments.length == 0
+  end 
 end
