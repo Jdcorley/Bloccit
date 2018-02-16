c4a7ee8d1aacbfa1e340a9a96cf3333e0aa5b2b0
Jack <jdcorley@gmail.com>
2018-02-16 18:43:49 -0500

    checkpoint 31-corrections (render favorites)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,13 @@
 module UsersHelper
-  
-  def current_user_posts_none
-    current_user.posts.length == 0 
-  end 
+  def current_user_posts_none?
+    current_user.posts.empty?
+  end
 
-  def current_user_comments_none
-    current_user.comments.length == 0
-  end 
+  def current_user_comments_none?
+    current_user.comments.empty?
+  end
+
+  def current_user_favorites_none?
+    current_user.favorites.empty?
+  end
 end
