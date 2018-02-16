13f066562285cf7fed14705b59564301642d7e0a
Jack <jdcorley@gmail.com>
2018-02-16 18:43:49 -0500

    checkpoint 31-corrections (render favorites)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,29 +5,29 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   before_save { self.email = email.downcase if email.present? }
-  before_save { self.name = if name.present? then  name.split(" ").each.map do |n| n.capitalize end* ' '; end }
+  before_save { self.name = if name.present? then name.split(' ').each.map(&:capitalize) * ' '; end }
   before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, presence: true, length: { minimum: 6 }, if: 'password_digest.nil?'
   validates :password, length: { minimum: 6 }, allow_blank: true
 
   validates :email,
-    presence: true,
-    uniqueness: { case_sensitive: false },
-    length: { minimum: 3, maximum: 254 }
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 254 }
 
   has_secure_password
 
-  enum role: [:member, :admin]
+  enum role: %i[member admin]
 
   def favorite_for(post)
     favorites.where(post_id: post.id).first
   end
-  
+
   def avatar_url(size)
-    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    gravatar_id = Digest::MD5.hexdigest(email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
 end
