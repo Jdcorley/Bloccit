cfeeee445a9ffaa76900c841212add342df5d054
Jack <jdcorley@gmail.com>
2018-02-18 13:52:00 -0500

    mailer template correct2

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,22 +2,21 @@ class FavoriteMailer < ApplicationMailer
   default from: 'jdcorley@gmail.com'
 
   def new_comment(user, post, comment)
-
-    headers["Message-ID"] = "<comments/#{comment.id}@bloccit.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
-    headers["References"] = "<post/#{post.id}@bloccit.example>"
+    headers['Message-ID'] = "<comments/#{comment.id}@bloccit.example>"
+    headers['In-Reply-To'] = "<post/#{post.id}@bloccit.example>"
+    headers['References'] = "<post/#{post.id}@bloccit.example>"
 
     @user = user
     @post = post
     @comment = comment
 
-    mail(to: user.email, subject: "New comment on #{post.title}"")
+    mail(to: user.email, subject: "New comment on #{post.title}")
   end
 
   def new_post(post)
-    headers["Message-ID"] = "<comments/#{comment.id}@bloccit.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
-    headers["References"] = "<post/#{post.id}@bloccit.example>"
+    headers['Message-ID'] = "<comments/#{comment.id}@bloccit.example>"
+    headers['In-Reply-To'] = "<post/#{post.id}@bloccit.example>"
+    headers['References'] = "<post/#{post.id}@bloccit.example>"
 
     @post = post
 
